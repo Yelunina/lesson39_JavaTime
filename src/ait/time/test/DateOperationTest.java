0b37c2d81388c5261f6b887db30930d814cc6835
Iryna Yelunina <lucky.asterisk@gmail.com>
2023-10-31 19:24:40 +0100

    code

--- src/ait/time/test/DateOperationTest.java
+++ src/ait/time/test/DateOperationTest.java
@@ -1,20 +1,23 @@
 package ait.time.test;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
+
+import ait.time.utils.DateOperation;
+import org.junit.jupiter.api.Test;
 
 class DateOperationTest {
 
-    @org.junit.jupiter.api.BeforeEach
-    void setUp() {
-    }
-
-    @org.junit.jupiter.api.Test
+    @Test
     void getAge() {
+        assertEquals(62, DateOperation.getAge("12/04/1961"));
+        assertEquals(61, DateOperation.getAge("1961-11-28"));
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void sortStringDates() {
+        String[] dates = {"2000-12-01", "10/12/2000", "1970-08-12", "2010-10-05"};
+        String[] actual = DateOperation.sortStringDates(dates);
+        String[] expected = {"1970-08-12", "2000-12-01", "10/12/2000", "2010-10-05"};
+        assertArrayEquals(expected, actual);
     }
 }
