7192c042816b2c798d80f04df105ed95d7bb137d
Iryna Yelunina <lucky.asterisk@gmail.com>
2023-10-31 19:12:17 +0100

    init commit

---
+++ src/ait/time/test/DateOperationTest.java
@@ -0,0 +1,20 @@
+package ait.time.test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DateOperationTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getAge() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void sortStringDates() {
+    }
+}
